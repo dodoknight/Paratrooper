1ca747f4eda542619603390e3d547157c799bcd6
Adam Vesecký <vesecky.adam@gmail.com>
2018-01-18 17:54:33 +0100

    Add paratrooper assets and components

---
+++ src/ParatrooperFactory.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Component.h"
+
+using namespace Cog;
+
+// forward decl
+class Cog::Node;
+class ParatrooperModel;
+
+/**
+ * Game object factory
+ */
+class ParatrooperFactory : public Component {
+public:
+
+	ParatrooperModel* LoadGameModel();
+
+	void InitializeGame(Cog::Node* rootObject, ParatrooperModel* model);
+
+	void CreateProjectile(Cog::Node* canon, ParatrooperModel* model);
+
+	void CreateParatrooper(Cog::Node* owner, ParatrooperModel* model);
+
+	void CreateCopter(Cog::Node* owner, ParatrooperModel* model);
+
+	virtual void Update(const uint64 delta, const uint64 absolute) {
+
+	}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
 #include "ofxCogMain.h"
 #include "jsonxx.h"
 #include "pugixml.h"
-
+#include "ParatrooperFactory.h"
 
 
 class ParatrooperApp : public ofxCogApp {
 
 
 	void RegisterComponents() {
-		REGISTER_BEHAVIOR(NoahBeOver);
-		REGISTER_BEHAVIOR(NoahLoading);
+		REGISTER_COMPONENT(new ParatrooperFactory());
 	}
 
 
 	void InitComponents() {	
-	
+		auto factory = GETCOMPONENT(ParatrooperFactory);
+		auto model = factory->LoadGameModel();
+		//factory->InitializeGame(root, model);
 	}
 
 	void InitStage(Stage* stage) {
 		ofxCogEngine::GetInstance().LoadStage();
-		stage->GetRootObject()->AddBehavior(new BackButtonKey());
 	}
 };
 
